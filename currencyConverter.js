5826e393e3d9b9342a41b84b984ffab0e4412b4b
Heshan <heshanpun@gmail.com>
2022-03-08 23:11:45 -0700

    made some changes, added ration

--- currencyConverter.js
+++ currencyConverter.js
@@ -5,91 +5,162 @@ function convertCurrency(){
     var toCurrency = document.getElementById("toUnit").value;
     var currency;
     var result;
-    
-    
+    var ratio;
     
     switch(true){
+        // from EURO
+        case ((fromCurrency == "fr-eur") && (toCurrency == "to-eur")):{
+            ratio = 1;
+            result = userValue*ratio;
+            currency = " &euro;";
+            
+            break;
+        }
+        case ((fromCurrency == "fr-eur") && (toCurrency == "to-usd")):{
+            ratio = 1.09;
+            result = userValue*ratio;
+            currency = " $USD";
+            break;
+        }
+        case ((fromCurrency == "fr-eur") && (toCurrency == "to-cdn")):{
+            ratio = 1.41;
+            result = userValue*ratio;
+            currency = " $CDN";
+            break;
+        }
+        case ((fromCurrency == "fr-eur") && (toCurrency == "to-btc")):{
+            ratio = 0.000026;
+            result = userValue*ratio;
+            currency = " &#8383;";
+            break;
+        }
+        case ((fromCurrency == "fr-eur") && (toCurrency == "to-eth")):{
+            ratio = 0.0004196;
+            result = userValue*ratio;
+            currency = " ETH";
+            break;
+        }
         // from USD
+        case ((fromCurrency == "fr-usd") && (toCurrency == "to-eur")):{
+            ratio = 0.92;
+            result = userValue * ratio;
+            currency = " &euro;";
+            break;
+        }
         case ((fromCurrency == "fr-usd") && (toCurrency == "to-usd")):{
-            result = userValue*1;
+            ratio = 1;
+            result = userValue*ratio;
             currency = " $USD";
             break;
         }
         case ((fromCurrency == "fr-usd") && (toCurrency == "to-cdn")):{
-            result = userValue * 1.29;
+            ratio = 1.29;
+            result = userValue * ratio;
             currency = " $CDN";
             break;
         }
         case ((fromCurrency == "fr-usd") && (toCurrency == "to-btc")):{
-            result = userValue * 0.000024;
-            currency = " BTC";
+            ratio = 0.000024;
+            result = userValue * ratio;
+            currency = " &#8383;";
             break;
         }
         case ((fromCurrency == "fr-usd") && (toCurrency == "to-eth")):{
-            result = userValue * 0.00037;
+            ratio = 0.00037;
+            result = userValue * ratio;
             currency = " ETH";
             break;
         }
         // from CDN
+        case ((fromCurrency == "fr-cdn") && (toCurrency == "to-eur")):{
+            ratio = 0.71;
+            result = userValue * 0.71;
+            currency = " &euro;";
+            break;
+        }
         case ((fromCurrency == "fr-cdn") && (toCurrency == "to-usd")):{
-            result = userValue * 0.78;
+            ratio = 0.78;
+            result = userValue * ratio;
             currency = " $USD";
             break;
         }
         case ((fromCurrency == "fr-cdn") && (toCurrency == "to-cdn")):{
-            result = userValue*1;
+            ratio = 1;
+            result = userValue*ratio;
             currency = " $CDN";
             break;
         }
         case ((fromCurrency == "fr-cdn") && (toCurrency == "to-btc")):{
-            result = userValue * 0.000019;
-            currency = " BTC";
+            ratio = 0.000019;
+            result = userValue * ratio;
+            currency = " &#8383;";
             break;
         }
         case ((fromCurrency == "fr-cdn") && (toCurrency == "to-eth")):{
-            result = userValue * 0.00029;
+            ratio = 0.00029;
+            result = userValue * ratio;
             currency = " ETH";
             break;
         }
-        // from BTC
+        // from BTC;
+        case ((fromCurrency == "fr-btc") && (toCurrency == "to-eur")):{
+            ratio =  37986.35;
+            result = userValue * ratio;
+            currency = " &euro;";
+            break;
+        }
         case ((fromCurrency == "fr-btc") && (toCurrency == "to-usd")):{
-            result = userValue * 41844.30;
+            ratio = 41844.30;
+            result = userValue * ratio;
             currency = " $USD";
             break;
         }
         case ((fromCurrency == "fr-btc") && (toCurrency == "to-cdn")):{
-            result = userValue*53900.69;
+            ratio = 53900.69;
+            result = userValue*ratio;
             currency = " $CDN";
             break;
         }
         case ((fromCurrency == "fr-btc") && (toCurrency == "to-btc")):{
-            result = userValue * 1;
-            currency = " BTC";
+            ratio = 1;
+            result = userValue * ratio;
+            currency = " &#8383";
             break;
         }
         case ((fromCurrency == "fr-btc") && (toCurrency == "to-eth")):{
-            result = userValue * 15.06;
+            ratio = 15.06;
+            result = userValue * ratio;
             currency = " ETH";
             break;
         }
         //from ETH
+        case ((fromCurrency == "fr-eth") && (toCurrency == "to-eur")):{
+            ratio = 2426.42;
+            result = userValue * ratio;
+            currency = " &euro;";
+            break;
+        }
         case ((fromCurrency == "fr-eth") && (toCurrency == "to-usd")):{
-            result = userValue * 2716.44;
+            ratio = 2716.44;
+            result = userValue * ratio;
             currency = " $USD";
             break;
         }
         case ((fromCurrency == "fr-eth") && (toCurrency == "to-cdn")):{
-            result = userValue*3498.15;
+            ratio = 3498.15;
+            result = userValue* ratio;
             currency = " $CDN";
             break;
         }
         case ((fromCurrency == "fr-eth") && (toCurrency == "to-btc")):{
-            result = userValue * 0.06608;
-            currency = " BTC";
+            ratio =  0.06608;
+            result = userValue * ratio;
+            currency = " &#8383";
             break;
         }
         case ((fromCurrency == "fr-eth") && (toCurrency == "to-eth")):{
-            result = userValue * 1;
+            ratio = 1;
+            result = userValue * ratio;
             currency = " ETH";
             break;
         }
@@ -97,8 +168,8 @@ function convertCurrency(){
         
     }
     
-    document.getElementById("resultOut").innerHTML = "<hr>" + result.toFixed(2) + currency;
-    
+    document.getElementById("resultOut").innerHTML = "<hr>" + result.toFixed(2) + currency + " (1:" + ratio + ")";
+    //ratio makes more sense when its 1:1.22 for example, user currency:converted
     
     
 }
